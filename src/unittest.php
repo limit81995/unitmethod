8c43356ca451d5900a0a9856c732a479adf69dce
limit81995 <63061177+limit81995@users.noreply.github.com>
2022-06-08 16:09:19 +0800

    Update unittest.php

--- src/unittest.php
+++ src/unittest.php
@@ -2,7 +2,19 @@
 namespace Limit81995\UnitMethod;
 
 class unittest{
-    public function index(){
+    public function index(){a
+        asdf
+        a
+        sdg
+        asdgasdg
+        aw
+        erg
         echo "hello composer autol123123o1111ad!";
+                            adfhadfh
+                                adfh
+                                ad
+                                fh
+                                adfh
+                                
     }
 }
